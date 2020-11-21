083255db4534a62bbe603d37e0526badbac10c01
Matan Steinmetz <matanabc@gmail.com>
2020-11-21 21:56:41 +0200

    Add: Setpoint to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as Modifier from './modifier/modifier';
 import PathConfig from './path-config';
 import Waypoint from './waypoint';
+import Setpoint from './setpoint';
 import * as Util from './util';
 import Path from './path';
 
-export { Modifier, PathConfig, Waypoint, Path, Util };
+export { Modifier, PathConfig, Waypoint, Path, Util, Setpoint };

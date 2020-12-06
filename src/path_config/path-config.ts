eef1ca3ba73f502c07651b6db613940a7955c79d
Matan Steinmetz <matanabc@gmail.com>
2020-12-06 19:46:51 +0200

    Remove: PathConfig copy constructor

--- src/path_config/path-config.ts
+++ src/path_config/path-config.ts
@@ -1,28 +1,13 @@
 export default class PathConfig {
-	acc: number = 2;
-	vMax: number = 2;
-	width: number = 0.6;
-	robotLoopTime: number = 0.02;
+	acc: number;
+	vMax: number;
+	width: number;
+	robotLoopTime: number;
 
-	constructor(
-		widthOrPathConfig: number | PathConfig = 0,
-		vMax: number = 0,
-		acc: number = 0,
-		robotLoopTime: number = 0.02
-	) {
-		if (widthOrPathConfig instanceof PathConfig) this.copyConstructor(widthOrPathConfig);
-		else {
-			this.acc = acc;
-			this.vMax = vMax;
-			this.width = widthOrPathConfig;
-			this.robotLoopTime = robotLoopTime;
-		}
-	}
-
-	private copyConstructor(pathConfig: PathConfig) {
-		this.acc = pathConfig.acc ? pathConfig.acc : this.acc;
-		this.vMax = pathConfig.vMax ? pathConfig.vMax : this.vMax;
-		this.width = pathConfig.width ? pathConfig.width : this.width;
-		this.robotLoopTime = pathConfig.robotLoopTime ? pathConfig.robotLoopTime : this.robotLoopTime;
+	constructor(width: number = 0, vMax: number = 0, acc: number = 0, robotLoopTime: number = 0.02) {
+		this.acc = acc;
+		this.vMax = vMax;
+		this.width = width;
+		this.robotLoopTime = robotLoopTime;
 	}
 }

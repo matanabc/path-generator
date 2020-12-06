71887f51ef950ff8996e4acb31d85ae3049b35d9
Matan Steinmetz <matanabc@gmail.com>
2020-12-06 19:44:40 +0200

    Clean: Setpoint

--- src/setpoint.ts
+++ src/setpoint.ts
@@ -1,7 +1,7 @@
 export default class Setpoint {
-	position: number = 0;
-	velocity: number = 0;
-	acceleration: number = 0;
+	position: number;
+	velocity: number;
+	acceleration: number;
 
 	constructor(position: number = 0, velocity: number = 0, acceleration: number = 0) {
 		this.position = position;

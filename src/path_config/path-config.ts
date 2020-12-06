53b08d09e6a71ba40b98beefeff459319daf5902
Matan Steinmetz <matanabc@gmail.com>
2020-12-06 19:51:06 +0200

    Add: getter property to path config

--- src/path_config/path-config.ts
+++ src/path_config/path-config.ts
@@ -1,13 +1,29 @@
 export default class PathConfig {
-	acc: number;
-	vMax: number;
-	width: number;
-	robotLoopTime: number;
+	private _acc: number;
+	private _vMax: number;
+	private _width: number;
+	private _robotLoopTime: number;
 
 	constructor(width: number = 0, vMax: number = 0, acc: number = 0, robotLoopTime: number = 0.02) {
-		this.acc = acc;
-		this.vMax = vMax;
-		this.width = width;
-		this.robotLoopTime = robotLoopTime;
+		this._acc = acc;
+		this._vMax = vMax;
+		this._width = width;
+		this._robotLoopTime = robotLoopTime;
+	}
+
+	get acc(): number {
+		return this._acc;
+	}
+
+	get vMax(): number {
+		return this._vMax;
+	}
+
+	get width(): number {
+		return this._width;
+	}
+
+	get robotLoopTime(): number {
+		return this._robotLoopTime;
 	}
 }

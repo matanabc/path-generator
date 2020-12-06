785b4991420909d0e35aee267cb4818a890a3cd0
Matan Steinmetz <matanabc@gmail.com>
2020-12-06 20:24:53 +0200

    Update: setpoint

--- src/setpoint.ts
+++ src/setpoint.ts
@@ -1,11 +1,35 @@
 export default class Setpoint {
-	position: number;
-	velocity: number;
-	acceleration: number;
+	protected _position: number;
+	protected _velocity: number;
+	protected _acceleration: number;
 
 	constructor(position: number = 0, velocity: number = 0, acceleration: number = 0) {
-		this.position = position;
-		this.velocity = velocity;
-		this.acceleration = acceleration;
+		this._position = position;
+		this._velocity = velocity;
+		this._acceleration = acceleration;
+	}
+
+	get position(): number {
+		return this._position;
+	}
+
+	set position(position: number) {
+		this._position = position;
+	}
+
+	get velocity(): number {
+		return this._velocity;
+	}
+
+	set velocity(velocity: number) {
+		this._velocity = velocity;
+	}
+
+	get acceleration(): number {
+		return this._acceleration;
+	}
+
+	set acceleration(acceleration: number) {
+		this._acceleration = acceleration;
 	}
 }

7da965ea3118f0759f26952a439cad60360b8560
Matan Steinmetz <matanabc@gmail.com>
2020-12-06 19:38:20 +0200

    Remove: Waypoint copy constructor

--- src/waypoints/waypoint.ts
+++ src/waypoints/waypoint.ts
@@ -1,33 +1,16 @@
 export default class Waypoint {
-	x: number = 0;
-	y: number = 0;
-	angle: number = 0;
-	v: number = 0;
-	vMax: number = 2;
+	x: number;
+	y: number;
+	v: number;
+	vMax: number;
+	angle: number;
 
-	constructor(
-		xOrWaypoint: number | Waypoint = 0,
-		y: number = 0,
-		angle: number = 0,
-		v: number = 0,
-		vMax: number = 2
-	) {
-		if (xOrWaypoint instanceof Waypoint) this.copyConstructor(xOrWaypoint);
-		else {
-			this.x = xOrWaypoint;
-			this.angle = angle;
-			this.vMax = vMax;
-			this.y = y;
-			this.v = v;
-		}
-	}
-
-	private copyConstructor(waypoint: Waypoint): void {
-		this.angle = waypoint.angle ? waypoint.angle : this.angle;
-		this.vMax = waypoint.vMax ? waypoint.vMax : this.vMax;
-		this.x = waypoint.x ? waypoint.x : this.x;
-		this.y = waypoint.y ? waypoint.y : this.y;
-		this.v = waypoint.v ? waypoint.v : this.v;
+	constructor(x: number = 0, y: number = 0, angle: number = 0, v: number = 0, vMax: number = 0) {
+		this.x = x;
+		this.angle = angle;
+		this.vMax = vMax;
+		this.y = y;
+		this.v = v;
 	}
 
 	getInfo(): string {

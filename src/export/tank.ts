8f1eedb4ecf6990da9b76e336013d162bf72088f
Matan Steinmetz <matanabc@gmail.com>
2020-12-06 18:50:06 +0200

    Move: export Tank to index

--- src/export/tank.ts
+++
@@ -1,5 +0,0 @@
-import PathConfig from '../path_config/path-config';
-import Waypoint from '../waypoints/waypoint';
-import TankPath from '../path/tank-path';
-
-export { PathConfig, Waypoint, TankPath as Path };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import PathConfig from './path_config/path-config';
 import Waypoint from './waypoints/waypoint';
-import * as Tank from './export/tank';
 import Setpoint from './setpoint';
 import * as Util from './util';
 import Path from './path/path';
+import TankPath from './path/tank-path';
+
+const Tank = {
+	Path: TankPath,
+	PathConfig,
+	Waypoint,
+};
 
 export { PathConfig, Waypoint, Path, Util, Setpoint, Tank };

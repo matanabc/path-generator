8ecd2ecfb34e3b07fbbb1279ff36fdb932a7528b
Matan Steinmetz <matanabc@gmail.com>
2020-12-08 21:09:15 +0200

    Add: SwerveSetpoint

---
+++ src/setpoint/swerve-setpoint.ts
@@ -0,0 +1,19 @@
+import Setpoint from './setpoint';
+
+export default class SwerveSetpoint extends Setpoint {
+	protected _angle: number;
+
+	constructor(
+		position: number = 0,
+		velocity: number = 0,
+		acceleration: number = 0,
+		angle: number = 0
+	) {
+		super(position, velocity, acceleration);
+		this._angle = angle;
+	}
+
+	get angle(): number {
+		return this._angle;
+	}
+}

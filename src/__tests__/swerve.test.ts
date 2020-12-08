920ad3ee4f7e427630238c59b408807ada2ced16
Matan Steinmetz <matanabc@gmail.com>
2020-12-08 21:47:20 +0200

    Update: coord radios

--- src/__tests__/swerve.test.ts
+++ src/__tests__/swerve.test.ts
@@ -5,7 +5,7 @@ const pathConfig = new PathConfig(0.8, 2, 3);
 test('Straight path', () => {
 	const waypoints = [
 		new Swerve.Waypoint(0, 0, 0, 0, 0, 1),
-		new Swerve.Waypoint(0.5, 0.5, 90, 0, 0, 0),
+		new Swerve.Waypoint(0.5, 0.5, 90, 180, 0, 0),
 	];
 	const path = new Swerve.Path(waypoints, pathConfig);
 });
--- src/generator/swerve-path-generator.ts
+++ src/generator/swerve-path-generator.ts
@@ -1,9 +1,10 @@
+import SwerveWaypoint from '../waypoints/swerve-waypoint';
 import SwerveCoord from '../coord/swerve-coord';
 import PathGenerator from './path-generator';
 import PathConfig from '../path/path-config';
 import Waypoint from '../waypoints/waypoint';
-import Coord from '../coord/coord';
 import Setpoint from '../setpoint/setpoint';
+import Coord from '../coord/coord';
 import Spline from '../spline';
 import { d2r } from '../util';
 
@@ -14,7 +15,10 @@ export default class SwervePathGenerator extends PathGenerator {
 
 	protected generateCoords(spline: Spline, setpoints: Setpoint[], startPosition: number): Coord[] {
 		const coords = [];
-		const radios = spline.distance / d2r(180);
+		const robotStartAngle = (<SwerveWaypoint>spline.startPoint).robotAngle;
+		const robotEndAngle = (<SwerveWaypoint>spline.endPoint).robotAngle;
+		const robotTurnAngle = robotEndAngle - robotStartAngle;
+		const radios = robotTurnAngle !== 0 ? spline.distance / d2r(robotTurnAngle) : 0;
 		for (let i = 0; i < setpoints.length; i++) {
 			const coord = spline.getPositionCoords(setpoints[i].position - startPosition);
 			coords.push(new SwerveCoord(coord.x, coord.y, coord.angle, radios));

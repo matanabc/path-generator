a7c968f035739b826f4e5189272fda5d82d92dc4
Matan Steinmetz <matanabc@gmail.com>
2020-12-08 20:59:31 +0200

    Add: SwerveModifier

---
+++ src/modifier/swerve-modifier.ts
@@ -0,0 +1,15 @@
+import SwerveCoord from '../coord/swerve-coord';
+import PathConfig from '../path/path-config';
+import Setpoint from '../setpoint';
+
+export default class SwerveModifier {
+	constructor(sourceSetpoints: Setpoint[], coords: SwerveCoord[], pathConfig: PathConfig) {
+		this.modify(sourceSetpoints, coords, pathConfig);
+	}
+
+	protected modify(
+		sourceSetpoints: Setpoint[],
+		coords: SwerveCoord[],
+		pathConfig: PathConfig
+	): void {}
+}

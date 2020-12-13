6671c8791c77f1879f8f0e5f2733b4def700f7ac
Matan Steinmetz <matanabc@gmail.com>
2020-12-13 09:36:57 +0200

    Update: TankPath

--- src/path/tank-path.ts
+++ src/path/tank-path.ts
@@ -1,26 +1,22 @@
 import TankModifier from '../modifier/tank-modifier';
 import Waypoint from '../waypoints/waypoint';
-import PathConfig from './path-config';
 import Setpoint from '../setpoint/setpoint';
+import PathConfig from './path-config';
 import Path from './path';
 
 export default class TankPath extends Path {
-	protected tankModifier: TankModifier = {} as TankModifier;
+	protected modifier: TankModifier;
 
 	constructor(waypoints: Waypoint[], pathConfig: PathConfig) {
 		super(waypoints, pathConfig);
-	}
-
-	protected generate(): void {
-		super.generate();
-		this.tankModifier = new TankModifier(this.sourceSetpoints, this.coords, this.pathConfig);
+		this.modifier = new TankModifier(this.sourceSetpoints, this.coords, this.pathConfig);
 	}
 
 	get leftSetpoints(): Setpoint[] {
-		return this.tankModifier.leftSetpoints;
+		return this.modifier.leftSetpoints;
 	}
 
 	get rightSetpoints(): Setpoint[] {
-		return this.tankModifier.rightSetpoints;
+		return this.modifier.rightSetpoints;
 	}
 }

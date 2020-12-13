4d7cf1a97b354382f8b84b29526319f008876819
Matan Steinmetz <matanabc@gmail.com>
2020-12-13 11:08:54 +0200

    Update: tank turn in place tests

--- src/__tests__/tank.test.ts
+++ src/__tests__/tank.test.ts
@@ -1,4 +1,4 @@
-import { Tank } from '../index';
+import { Tank, Util } from '../index';
 import TankPath from '../path/tank-path';
 const { Path, PathConfig, Waypoint } = Tank;
 
@@ -38,15 +38,19 @@ test('Turn in place path right', () => {
 	const waypoints = [new Waypoint(0, 0, 0, 0, 2.5), new Waypoint(0, 0, 90, 0, 0)];
 	const path = new Path(waypoints, pathConfig);
 	basicPathCheck(path);
+	expect(Util.r2d(path.coords[0].angle)).toBeCloseTo(waypoints[0].angle, 0.05);
+	expect(Util.r2d(path.coords[path.coords.length - 1].angle)).toBeCloseTo(waypoints[1].angle, 0.05);
 	for (let i = 0; i < path.sourceSetpoints.length; i++) {
 		expect(path.leftSetpoints[i].position).toBeLessThanOrEqual(-path.rightSetpoints[i].position);
 	}
 });
 
 test('Turn in place path left', () => {
-	const waypoints = [new Waypoint(0, 0, 0, 0, 2.5), new Waypoint(0, 0, 90, 0, 0)];
+	const waypoints = [new Waypoint(0, 0, 0, 0, 2.5), new Waypoint(0, 0, -90, 0, 0)];
 	const path = new Path(waypoints, pathConfig);
 	basicPathCheck(path);
+	expect(Util.r2d(path.coords[0].angle)).toBeCloseTo(waypoints[0].angle, 0.05);
+	expect(Util.r2d(path.coords[path.coords.length - 1].angle)).toBeCloseTo(waypoints[1].angle, 0.05);
 	for (let i = 0; i < path.sourceSetpoints.length; i++) {
 		expect(path.rightSetpoints[i].position).toBeLessThanOrEqual(-path.leftSetpoints[i].position);
 	}

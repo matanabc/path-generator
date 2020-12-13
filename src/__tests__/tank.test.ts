88b829ec9f2b9a3a33e2b0131098f973f40d71ec
Matan Steinmetz <matanabc@gmail.com>
2020-12-13 09:56:02 +0200

    Add: tank test

---
+++ src/__tests__/tank.test.ts
@@ -0,0 +1,53 @@
+import { Tank } from '../index';
+import TankPath from '../path/tank-path';
+const { Path, PathConfig, Waypoint } = Tank;
+
+const pathConfig = new PathConfig(0.8, 2, 3);
+
+function basicPathCheck(path: TankPath): void {
+	expect(path.isIllegal()).toBe(false);
+	expect(path.leftSetpoints.length).toBe(path.rightSetpoints.length);
+	expect(path.leftSetpoints.length).toBe(path.sourceSetpoints.length);
+}
+
+test('Straight path', () => {
+	const waypoints = [new Waypoint(0, 0, 0, 0, 2), new Waypoint(2, 0, 0, 0, 0)];
+	const path = new Path(waypoints, pathConfig);
+	basicPathCheck(path);
+});
+
+test('Turn path right', () => {
+	const waypoints = [new Waypoint(0, 0, 0, 0, 2), new Waypoint(1.5, 1.5, 90, 0, 0)];
+	const path = new Path(waypoints, pathConfig);
+	basicPathCheck(path);
+	for (let i = 0; i < path.sourceSetpoints.length; i++) {
+		expect(path.rightSetpoints[i].position).toBeLessThanOrEqual(path.leftSetpoints[i].position);
+	}
+});
+
+test('Turn path left', () => {
+	const waypoints = [new Waypoint(0, 0, 90, 0, 2), new Waypoint(1.5, 1.5, 0, 0, 0)];
+	const path = new Path(waypoints, pathConfig);
+	basicPathCheck(path);
+	for (let i = 0; i < path.sourceSetpoints.length; i++) {
+		expect(path.leftSetpoints[i].position).toBeLessThanOrEqual(path.rightSetpoints[i].position);
+	}
+});
+
+test('Turn in place path right', () => {
+	const waypoints = [new Waypoint(0, 0, 0, 0, 2.5), new Waypoint(0, 0, 90, 0, 0)];
+	const path = new Path(waypoints, pathConfig);
+	basicPathCheck(path);
+	for (let i = 0; i < path.sourceSetpoints.length; i++) {
+		expect(path.leftSetpoints[i].position).toBeLessThanOrEqual(-path.rightSetpoints[i].position);
+	}
+});
+
+test('Turn in place path left', () => {
+	const waypoints = [new Waypoint(0, 0, 0, 0, 2.5), new Waypoint(0, 0, 90, 0, 0)];
+	const path = new Path(waypoints, pathConfig);
+	basicPathCheck(path);
+	for (let i = 0; i < path.sourceSetpoints.length; i++) {
+		expect(path.rightSetpoints[i].position).toBeLessThanOrEqual(-path.leftSetpoints[i].position);
+	}
+});

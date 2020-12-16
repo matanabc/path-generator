bfda3881589efc3e366e2cf0d76c8e192979f79c
Matan Steinmetz <matanabc@gmail.com>
2020-12-16 16:29:48 +0200

    Add: change direction

--- src/setpoint/setpoint.ts
+++ src/setpoint/setpoint.ts
@@ -1,35 +1,17 @@
 export default class Setpoint {
-	protected _position: number;
-	protected _velocity: number;
-	protected _acceleration: number;
+	public position: number;
+	public velocity: number;
+	public acceleration: number;
 
 	constructor(position: number = 0, velocity: number = 0, acceleration: number = 0) {
-		this._position = position;
-		this._velocity = velocity;
-		this._acceleration = acceleration;
+		this.position = position;
+		this.velocity = velocity;
+		this.acceleration = acceleration;
 	}
 
-	get position(): number {
-		return this._position;
-	}
-
-	set position(position: number) {
-		this._position = position;
-	}
-
-	get velocity(): number {
-		return this._velocity;
-	}
-
-	set velocity(velocity: number) {
-		this._velocity = velocity;
-	}
-
-	get acceleration(): number {
-		return this._acceleration;
-	}
-
-	set acceleration(acceleration: number) {
-		this._acceleration = acceleration;
+	changeDirection(): void {
+		this.acceleration *= -1;
+		this.position *= -1;
+		this.velocity *= -1;
 	}
 }
